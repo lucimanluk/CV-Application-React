a9206c322aafa32b65bc56652e35fe065592e488
lucimanluk <lucimanluk4000@gmail.com>
2024-02-25 02:12:26 -0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Information from "./components/Information.jsx";
 
 function App() {
   return (
-    <div className="bg-slate-800 min-h-screen flex-1 flex-col m-0 rounded-none">
+    <div className=" min-h-screen flex flex-col m-0 rounded-none bg-slate-800 gap-4 p-2">
       {/*{["general", "education", "experience"].map((item) => <Information name = item></Information>)}*/}
       <Information name="general" />
       <Information name="education" />
--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,30 +1,39 @@
-import InputForm from "./InputForm";
+import InputForm from "./inputForm";
+import { useState } from "react";
 
 function Information({ name }) {
-  if (name === "general") return (
-  <div className="flex flex-col w-56">
-    <InputForm label = "First Name"/>
-    <InputForm label = "Last Name"/>  
-    <InputForm label = "Email"/>
-    <InputForm label = "Phone number"/>
-  </div>
-  )
-  else if (name === "education") return (
-    <div className="flex flex-col w-56">
-      <InputForm label = "high school"/>
-      <InputForm label = "University"/>  
-      <InputForm label = "Starting year"/>
-      <InputForm label = "Finishing year"/>
+
+  return (
+    <div
+      className="flex flex-col w-64 rounded-xl border-solid
+   border-white border-2 items-center justify-center p-2 gap-y-2"
+    >
+      {name === "general" ? (
+        <>
+          <InputForm label="First Name" />
+          <InputForm label="Last Name" />
+          <InputForm label="Age" />
+        <button onClick={handleClick}>{content}</button>
+        </>
+      ) : name === "education" ? (
+        <>
+          <InputForm label="Highschool" />
+          <InputForm label="University name" />
+          <InputForm label="Starting year" />
+          <InputForm label="Finishing year" />
+          <button onClick={handleClick}>{content}</button>
+        </>
+      ) : name === "experience" ? (
+        <>
+          <InputForm label="Company name" />
+          <InputForm label="Starting year" />
+          <InputForm label="Finishing year" />
+          <InputForm label="Position" />
+          <button onClick={handleClick}></button>
+        </>
+      ) : null}
     </div>
-    )
-   else if (name === "experience") return (
-      <div className="flex flex-col w-56">
-        <InputForm label = "First job"/>
-        <InputForm label = "Last Name"/>  
-        <InputForm label = "Email"/>
-        <InputForm label = "Phone number"/>
-      </div>
-      )
+  );
 }
 
 export default Information;
