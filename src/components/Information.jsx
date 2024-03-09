b1e5cd06a17b6f357ac955b60db862ba0340395c
lucimanluk <lucimanluk4000@gmail.com>
2024-03-09 20:54:21 +0200

    Created the useStates to get the content of the input fields. Modified the select to get the value from the number state variable. Clear button works now on <select> from InputForm.

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -3,22 +3,59 @@ import Preview from "./Preview";
 import SaveClear from "./SaveClear";
 import { useState } from "react";
 
-
 function Information({ name }) {
   const [value, setValue] = useState(false);
-  const [string, setString] = useState('');
- 
-  function handleChange(e) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [string4, setString4] = useState("");
+  const [string5, setString5] = useState("");
+  const [number, setNumber] = useState(1900);
+
+  function handleChange1(e) {
     console.log(e.target.value);
-    setString(e.target.value);
+    setFirstName(e.target.value);
   }
 
-  function handleClick2(e) {
-    setString('');
+  function handleChange2(e) {
+    console.log(e.target.value);
+    setLastName(e.target.value);
+  }
+
+  function handleChange3(e) {
+    console.log(e.target.value);
+    setAge(e.target.value);
+  }
+
+  function handleChange4(e) {
+    console.log(e.target.value);
+    setString4(e.target.value);
+  }
+
+  function handleChange5(e) {
+    console.log(e.target.value);
+    setString5(e.target.value);
+  }
+
+  function handleChange6(e) {
+    console.log(e.target.value);
+    setNumber(e.target.value);
   }
 
   function handleClick() {
     setValue(!value);
+  }
+
+  function handleClick2() {
+    setFirstName("");
+    setLastName("");
+    setAge("");
+  }
+
+  function handleClick3() {
+    setString4("");
+    setString5("");
+    setNumber(1900);
   }
 
   return (
@@ -30,56 +67,102 @@ function Information({ name }) {
         <>
           <Preview name="General" value={value} handleClick={handleClick} />
 
-          {
-            value === true ? (
-              <>
-                {["First Name",
-                  "Last Name",
-                  "Age"
-                ].map((item, index) => {
-                  return <InputForm key={index} label={item} string = {string} handleChange={handleChange}/>
-                })}
-                <SaveClear handleClick = {handleClick2}/>
-              </>
-            ) : null
-          }
+          {value === true ? (
+            <>
+              {["First Name", "Last Name", "Age"].map((item, index) => {
+                if (index === 0) {
+                  return (
+                    <InputForm
+                      key={index}
+                      label={item}
+                      string={firstName}
+                      handleChange={handleChange1}
+                    />
+                  );
+                } else if (index === 1) {
+                  return (
+                    <InputForm
+                      key={index}
+                      label={item}
+                      string={lastName}
+                      handleChange={handleChange2}
+                    />
+                  );
+                } else if (index === 2) {
+                  return (
+                    <InputForm
+                      key={index}
+                      label={item}
+                      string={age}
+                      handleChange={handleChange3}
+                    />
+                  );
+                }
+              })}
+              <SaveClear handleClick={handleClick2} />
+            </>
+          ) : null}
         </>
       ) : name === "education" ? (
         <>
           <Preview name="Education" value={value} handleClick={handleClick} />
-          {
-            value === true ? (
-              <>
-                {[
-                  "Highschool",
-                  "University name",
-                  "Starting year",
-                  "Finishing year",
-                ].map((item, index) => {
-                  return <InputForm key = {index} label={item} />;
-                })}
-                <SaveClear/>
-              </>
-            ) : null}
+          {value === true ? (
+            <>
+              {[
+                "Highschool",
+                "University name",
+                "Starting year",
+                "Finishing year",
+              ].map((item, index) => {
+                if (index === 0) {
+                  return (
+                    <InputForm
+                      key={index}
+                      label={item}
+                      string={string4}
+                      handleChange={handleChange4}
+                    />
+                  );
+                } else if (index === 1) {
+                  return (
+                    <InputForm
+                      key={index}
+                      label={item}
+                      string={string5}
+                      handleChange={handleChange5}
+                    />
+                  );
+                } else if (index === 2) {
+                  return (
+                    <InputForm
+                      key={index}
+                      label={item}
+                      number={number}
+                      handleNumberChange={handleChange6}
+                    />
+                  );
+                }
+              })}
+              <SaveClear handleClick={handleClick3}/>
+            </>
+          ) : null}
         </>
       ) : name === "experience" ? (
         <>
           <Preview name="Experience" value={value} handleClick={handleClick} />
-          {
-            value === true ? (
-              <>
-                {[
-                  "Company name",
-                  "Starting year",
-                  "Finishing year",
-                  "Position"
-                ].map(
-                  (item, index) => {
-                    return <InputForm key = {index} label={item} />;
-                  })}
-                <SaveClear />
-              </>
-            ) : null}
+          {value === true ? (
+            <>
+              {[
+                "Company name",
+                "Starting year",
+                "Finishing year",
+                "Position",
+              ].map((item, index) => {
+                return <InputForm key={index} label={item} />;
+              })}
+              <SaveClear />
+            </>
+          ) : null}
         </>
       ) : null}
     </div>
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,18 +1,18 @@
-function InputForm({label, handleChange, string}) {
+function InputForm({label, handleChange, string, number, handleNumberChange}) {
   return (
     <form className="flex flex-col w-56">
       <label htmlFor={label} className="text-white">
         {label}
       </label>
       {label === "Starting year" || label === "Finishing year" ? (
-        <select name="year" id="year" className="rounded-md">
+        <select name="year" id="year" className="rounded-md" value = {number} onChange = {handleNumberChange}>
           {[...Array(2025 - 1900)].map((_, index) => {
             const year = 1900 + index;
-            return <option key={year}>{year}</option>;
+            return <option key={year} value={year}>{year}</option>;
           })}
         </select>
       ) : (
-        <input type="text" className="rounded-md px-1" id={label}  onChange={handleChange}></input>
+        <input type="text" className="rounded-md px-1" id={label} value={string} onChange={handleChange}></input>
       )}
     </form>
   );
